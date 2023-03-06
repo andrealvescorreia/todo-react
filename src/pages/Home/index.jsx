11365cb66911910a2f2cbef4544f952e33d3cf3e
André Alves Correia <andre2013aguabranca@gmail.com>
2023-03-05 23:25:08 -0300

    New Task - first steps

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,55 @@
+import React, { useEffect, useState } from 'react'
 import { ToDoCard } from "../../components/ToDoCard"
 import './styles.css'
 export function Home() {
-  return (
-    <div className="home">
-        <ToDoCard name="Tarefa 1"></ToDoCard>
-        <ToDoCard name="Tarefa 2" description="Descricao"></ToDoCard>
-        <ToDoCard name="Tarefa 3 com um nome muito grande se ta maluco mermao nome grande do caraio azideia" description="Descricao"></ToDoCard>
-        <ToDoCard name="Tarefa 4" description="Uma descricao muito grande de uma tarefa que precisa ser feita o quanto antes! Mais texto é necessário para que essa caixa tenha mais de uma linha de texto."></ToDoCard>
-    </div>
+    const [tasks, setTasks] = useState([])
+    const [taskName, setTaskName] = useState('')
+
+    function handleNewTask(){
+        let newTask = {
+            name: taskName,
+            description: '',
+            completed: false,
+            timeOfInclusion: new Date().toLocaleDateString("pt-br", {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+            }),
+        }
+        setTasks(previousState => [...previousState, newTask])
+    }
+
+    function handleKeyDown(e){
+        if (e.key === 'Enter') {
+          setTaskName(e.target.value);
+          handleNewTask()
+          e.target.value = ''
+        }
+      };
+
+    return (
+        <div className="home">
+            <input
+                type="text"
+                placeholder="Add a task"
+                onChange={e => setTaskName(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+
+            <ToDoCard name="Tarefa 3 com um nome muito grande se ta maluco mermao nome grande do caraio azideia" description=""/>
+            <ToDoCard name="Tarefa 4" description="Uma descricao muito grande de uma tarefa que precisa ser feita o quanto antes! Mais texto é necessário para que essa caixa tenha mais de uma linha de texto."></ToDoCard>
+
+            {
+                tasks.map(task => 
+                    <ToDoCard  
+                        key={task.timeOfInclusion}
+                        name={task.name}
+                        description={task.description}
+                        completed={task.completed}
+                    />
+                )
+            }
+            
+        </div>
   )
 }
