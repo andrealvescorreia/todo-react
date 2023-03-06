7878ada1cf6f33a9295e2464cd6b5bf8549fa52a
André Alves Correia <andre2013aguabranca@gmail.com>
2023-03-05 22:14:56 -0300

    ToDoCard style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,10 @@ import './styles.css'
 export function Home() {
   return (
     <div className="home">
-        <ToDoCard></ToDoCard>
+        <ToDoCard name="Tarefa 1"></ToDoCard>
+        <ToDoCard name="Tarefa 2" description="Descricao"></ToDoCard>
+        <ToDoCard name="Tarefa 3 com um nome muito grande se ta maluco mermao nome grande do caraio azideia" description="Descricao"></ToDoCard>
+        <ToDoCard name="Tarefa 4" description="Uma descricao muito grande de uma tarefa que precisa ser feita o quanto antes! Mais texto é necessário para que essa caixa tenha mais de uma linha de texto."></ToDoCard>
     </div>
   )
 }
