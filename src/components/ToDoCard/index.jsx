92dbe0c91d60ab33a18b32a3193a5c961c904675
André Alves Correia <andre2013aguabranca@gmail.com>
2023-03-15 11:51:05 -0300

    task edit (with modal)

--- src/components/ToDoCard/index.jsx
+++ src/components/ToDoCard/index.jsx
@@ -1,12 +1,34 @@
 import './styles.css'
 import React, { useState, useEffect } from 'react'
 import { useTasksContext } from "../../providers/TasksProvider"
+import Modal from 'react-modal'
+import { PenSvg } from '../../assets/PenSvg'
+import { CheckSvg } from '../../assets/CheckSvg'
+Modal.setAppElement("#root")
 
 export function ToDoCard({ task }) {
     const [thisTask, setThisTask] = useState(task)
+    const [thisTaskEdited, setThisTaskEdited] = useState(thisTask)
 
     const {tasks, setTasks} = useTasksContext();
 
+    useEffect(() => {
+        updateThisTask()
+    }, [thisTask])
+
+    const [modalIsOpen, setIsOpen] = useState(false)
+    function openModal() { setIsOpen(true) }
+    function closeModal() { setIsOpen(false) }
+    
+    function saveEdit(){
+        setThisTask(thisTaskEdited)
+        closeModal()
+    }
+    function cancelEdit(){
+        setThisTaskEdited(thisTask)
+        closeModal()
+    }
+    
 
     
 
@@ -14,37 +36,72 @@ export function ToDoCard({ task }) {
         setThisTask({
             ...thisTask, 
             completed: !thisTask.completed
-        })   
+        }) 
     }
 
-    useEffect(() => {
+    function updateThisTask(){
         let thisTaskIndex = tasks.findIndex(taskItem => taskItem.timeOfInclusion === thisTask.timeOfInclusion)
-        let updatedTasks = [...tasks]
+        const updatedTasks = [...tasks]
         updatedTasks[thisTaskIndex] = thisTask
         setTasks(updatedTasks)
-    }, [thisTask])
+    }
+
+    function deleteThisTask(){
+        function isNotThisTask(taskItem){
+            return(taskItem.timeOfInclusion !== thisTask.timeOfInclusion)
+        }
+        const updatedTasks = [...tasks].filter(isNotThisTask);
+        setTasks(updatedTasks)
+    }
 
     return (
-        <div className={"card" + (thisTask.completed ? " completed" : "")}>
+        <div className={"card" + (thisTask.completed ? " completed" : "")} >
             <div className="card-header">
                 <button className="task-checkbox" type="button" onClick={handleCheckBoxClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
-                    </svg>
+                    <CheckSvg/>
                 </button>
 
                 <h2 className="task-name">{thisTask.name}</h2>
 
-                <button className="task-edit" type="button" >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
-                    </svg>
+                <button className="task-edit" type="button" onClick={openModal}>
+                    <PenSvg/>
                 </button>
             </div>
 
             <div className="card-body">
                 <p className="task-description">{thisTask.description}</p>
             </div>
+
+            <Modal 
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Modal louco"
+                overlayClassName="modal-overlay"
+                className="modal-content"
+            >
+                <input
+                    value={thisTaskEdited.name}
+                    type="text"
+                    onChange = {e => setThisTaskEdited({
+                            ...thisTaskEdited,
+                            name: e.target.value
+                        })
+                    }
+                />
+
+                <input
+                    value={thisTaskEdited.description}
+                    type="text"
+                    onChange = {e => setThisTaskEdited({
+                        ...thisTaskEdited,
+                        description: e.target.value
+                    })
+                }
+                />
+                <button onClick={deleteThisTask}>Deletar</button>
+                <button onClick={cancelEdit}>Cancelar</button>
+                <button onClick={saveEdit}>Salvar</button>
+            </Modal>
 
         </div>
     )
