dfd8af06d709eadc1930308d9964632f3f6f57f3
André Alves Correia <andre2013aguabranca@gmail.com>
2023-03-17 11:46:44 -0300

    fix base in wrong place

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-base: "/todo-react/"
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/todo-react/",
   plugins: [react()],
 })
